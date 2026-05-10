daef149b393cc2fa4ab9fde3446ddd5d5d1fae77
Mohamed Tharwat <mthawrat@example.com>
2026-05-10 18:46:56 +0300

    Fix: add preview config and allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,18 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
+
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
+    plugins: [react(), tailwindcss()],
+    preview: {
+        host: "0.0.0.0",
+        port: 3000,
+        allowedHosts: [
+            'crazy-taski-client-production.up.railway.app'
+        ]
+    },
+    server: {
+        host: "0.0.0.0",
+        port: 3000
+    }
 });
